0b8ad8caa7a86da0a3fcd67548298b1212e1ebca
Dunaev Kirill <60296337+Bloodmain@users.noreply.github.com>
2023-04-01 17:34:19 +0300

    Regions filter

--- analysis/api/urls.py
+++ analysis/api/urls.py
@@ -6,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('charts/<str:inn>', ChartsApi.as_view(), name='charts'),
     path('categories/', Categories.as_view(), name='categories'),
+    path('regions/', Regions.as_view(), name='regions'),
 ]
--- static/scripts/filters.js
+++ static/scripts/filters.js
@@ -1,8 +1,17 @@
 $.get('/api/categories', {}, function (data) {
-    let categories = data[0]['categories'];
+    let categories = data['categories'];
     let select_form = $('#list-categories');
     for (let i = 0; i < categories.length; ++i) {
         select_form.append('<option>' + categories[i] + '</option>');
+    }
+})
+
+$.get('/api/regions', {}, function (data) {
+    let regions = data['regions'];
+    console.log(regions)
+    let select_form = $('#list-regions');
+    for (let i = 0; i < regions.length; ++i) {
+        select_form.append('<option>' + regions[i] + '</option>');
     }
 })
 
@@ -12,5 +21,5 @@ $(document).ready(function () {
         ("0" + d.getDate()).slice(-2));
 })
 $('#applyFilters').click(function () {
-    let categories;
+    let categories = $("#categories-datalist")
 })
